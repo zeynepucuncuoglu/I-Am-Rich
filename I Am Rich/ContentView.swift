3759a9df6e1e13c6c51e8f717e04ddb7a906dfd1
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2023-12-07 17:03:40 +0300

    Text and image design practise using SwiftUI

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size:40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond").resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
         }
-        .padding()
+
     }
 }
 
