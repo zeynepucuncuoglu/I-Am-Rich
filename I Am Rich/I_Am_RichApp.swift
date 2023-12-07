a0518f4bd7dbd474023dd1cf08a177b4ee80ffb7
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2023-12-07 16:20:54 +0300

    Initial Commit

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Zeynep Üçüncüoğlu on 7.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
